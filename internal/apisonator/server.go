78c4509594b92180764d4f7bb6565abcf214b9a2
Philip Gough <philip.p.gough@gmail.com>
2020-04-22 17:21:01 +0100

    Limit the scope of the server to a subset of Apisonator

--- internal/apisonator/server.go
+++ internal/apisonator/server.go
@@ -1,34 +1,31 @@
 package apisonator
 
 import (
-	"encoding/xml"
 	"net/http"
-	"strconv"
+	"net/url"
 
 	"github.com/3scale/3scale-go-client/threescale"
 	"github.com/3scale/3scale-go-client/threescale/api"
+	convert "github.com/3scale/3scale-go-client/threescale/http"
 	"github.com/3scale/3scale-istio-adapter/pkg/threescale/backend"
 )
 
-type server struct {
+const (
+	rejectionHeader = "3scale-rejection-reason"
+)
+
+type Server struct {
 	router       *http.ServeMux
 	upstreamPeer threescale.Client
 }
 
-type authResponseXML struct {
-	Name       xml.Name `xml:",any"`
-	Authorized bool     `xml:"authorized,omitempty"`
-	Reason     string   `xml:"reason,omitempty"`
-	Code       string   `xml:"code,attr,omitempty"`
-}
-
-func newServer(upstream string) (*server, error) {
+func NewServer(upstream string) (*Server, error) {
 	backend, err := backend.NewBackend(upstream, nil)
 	if err != nil {
 		return nil, err
 	}
 
-	s := &server{
+	s := &Server{
 		router:       http.NewServeMux(),
 		upstreamPeer: backend,
 	}
@@ -36,59 +33,74 @@ func newServer(upstream string) (*server, error) {
 	return s, nil
 }
 
-func (s *server) registerRoutes() {
+func (s *Server) registerRoutes() {
 	s.router.HandleFunc("/transactions/authorize.xml", s.handleAuthorize())
 	s.router.HandleFunc("/transactions/authrep.xml", s.handleAuthRep())
 }
 
-func (s *server) handleAuthorize() http.HandlerFunc {
+func (s *Server) handleAuthorize() http.HandlerFunc {
 	return func(w http.ResponseWriter, request *http.Request) {
 		clientReq := convertRequest(request)
-
-		encoder := xml.NewEncoder(w)
-		resp := authResponseXML{
-			Authorized: true,
-			Code:       strconv.Itoa(http.StatusOK),
-		}
-
-		w.Header().Set("Content-Type", "application/xml")
-		if err := encoder.Encode(&resp); err != nil {
-			http.Error(w, "failed to encode to xml", http.StatusInternalServerError)
+		upstreamResp, err := s.upstreamPeer.Authorize(clientReq)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadGateway)
 			return
 		}
+
+		if !upstreamResp.Authorized {
+			statusCode := convert.CodeToStatusCode(upstreamResp.ErrorCode)
+			if statusCode == 0 {
+				statusCode = http.StatusNotImplemented
+			}
+			w.WriteHeader(statusCode)
+			w.Header().Set(rejectionHeader, upstreamResp.ErrorCode)
+			return
+
+		}
+		w.WriteHeader(http.StatusOK)
+		return
 	}
 }
 
-func (s *server) handleAuthRep() http.HandlerFunc {
+func (s *Server) handleAuthRep() http.HandlerFunc {
 	return func(w http.ResponseWriter, request *http.Request) {
 		clientReq := convertRequest(request)
-		encoder := xml.NewEncoder(w)
-		resp := authResponseXML{
-			Authorized: true,
-			Code:       strconv.Itoa(http.StatusOK),
-		}
-
-		w.Header().Set("Content-Type", "application/xml")
-		if err := encoder.Encode(&resp); err != nil {
-			http.Error(w, "failed to encode to xml", http.StatusInternalServerError)
+		upstreamResp, err := s.upstreamPeer.Authorize(clientReq)
+		if err != nil {
+			// using a generic bad gateway error here
+			http.Error(w, err.Error(), http.StatusBadGateway)
 			return
 		}
+
+		if !upstreamResp.Authorized {
+			statusCode := convert.CodeToStatusCode(upstreamResp.ErrorCode)
+			if statusCode == 0 {
+				statusCode = http.StatusNotImplemented
+			}
+			w.WriteHeader(statusCode)
+			w.Header().Set(rejectionHeader, upstreamResp.ErrorCode)
+			return
+
+		}
+		w.WriteHeader(http.StatusOK)
+		return
 	}
 }
 
 func convertRequest(request *http.Request) threescale.Request {
+	values, _ := url.ParseQuery(request.URL.RawQuery)
 	return threescale.Request{
-		Auth:    getClientAuthFromRequest(request),
-		Service: api.Service(request.URL.Query().Get("service_id")),
+		Auth:    getClientAuthFromRequest(values),
+		Service: api.Service(values.Get("service_id")),
 		Transactions: []api.Transaction{
 			{
 				Metrics: nil,
 				Params: api.Params{
-					AppID:    request.URL.Query().Get("app_id"),
-					AppKey:   request.URL.Query().Get("app_key"),
-					Referrer: request.URL.Query().Get("referrer"),
-					UserID:   request.URL.Query().Get("user_id"),
-					UserKey:  request.URL.Query().Get("user_key"),
+					AppID:    values.Get("app_id"),
+					AppKey:   values.Get("app_key"),
+					Referrer: values.Get("referrer"),
+					UserID:   values.Get("user_id"),
+					UserKey:  values.Get("user_key"),
 				},
 			},
 		},
@@ -96,8 +108,8 @@ func convertRequest(request *http.Request) threescale.Request {
 
 }
 
-func getClientAuthFromRequest(request *http.Request) api.ClientAuth {
-	if value := request.URL.Query().Get("service_token"); value != "" {
+func getClientAuthFromRequest(values url.Values) api.ClientAuth {
+	if value := values.Get("service_token"); value != "" {
 		return api.ClientAuth{
 			Type:  api.ServiceToken,
 			Value: value,
@@ -105,6 +117,6 @@ func getClientAuthFromRequest(request *http.Request) api.ClientAuth {
 	}
 	return api.ClientAuth{
 		Type:  api.ProviderKey,
-		Value: request.URL.Query().Get("provider_key"),
+		Value: values.Get("provider_key"),
 	}
 }
--- internal/apisonator/server_test.go
+++ internal/apisonator/server_test.go
@@ -1,44 +1,334 @@
 package apisonator
 
 import (
-	"encoding/xml"
+	"errors"
+	"fmt"
+	"github.com/3scale/3scale-istio-adapter/pkg/threescale/backend"
 	"net/http"
 	"net/http/httptest"
+	"net/url"
+	"path/filepath"
+	"reflect"
+	"runtime"
 	"testing"
+
+	"github.com/3scale/3scale-go-client/threescale"
+	"github.com/3scale/3scale-go-client/threescale/api"
+	convert "github.com/3scale/3scale-go-client/threescale/http"
+)
+
+const (
+	application  = "app"
+	service      = "service"
+	user         = "user"
+	serviceToken = "st"
+	providerKey  = "pk"
 )
 
 func Test_handleAuthorize(t *testing.T) {
-	srv := newServer()
-	req := httptest.NewRequest("GET", "/transactions/authorize.xml", nil)
-	w := httptest.NewRecorder()
-
-	srv.router.ServeHTTP(w, req)
-	if w.Code != http.StatusOK {
-		t.Errorf("unexpected status code, wanted 200 but got %d", w.Code)
+	tests := []struct {
+		name              string
+		queryBuilder      func() url.Values
+		upstream          *mockApisonator
+		expectCode        int
+		expectHeaderValue string
+	}{
+		{
+			name: "Test upstream error results in failure",
+			queryBuilder: func() url.Values {
+				values := url.Values{}
+				values.Add("app_id", application)
+				values.Add("service_id", service)
+				values.Add("service_token", serviceToken)
+				return values
+			},
+			upstream: &mockApisonator{
+				callback: func(request threescale.Request) {
+					expect := threescale.Request{
+						Auth: api.ClientAuth{
+							Type:  api.ServiceToken,
+							Value: serviceToken,
+						},
+						Service: service,
+						Transactions: []api.Transaction{
+							{
+								Params: api.Params{
+									AppID: application,
+								},
+							},
+						},
+					}
+					equals(t, expect, request)
+				},
+				err: errors.New("some error"),
+			},
+			expectCode: http.StatusBadGateway,
+		},
+		{
+			name: "Test upstream denial fails to authorize",
+			queryBuilder: func() url.Values {
+				values := url.Values{}
+				values.Add("user_key", user)
+				values.Add("service_id", service)
+				values.Add("provider_key", providerKey)
+				return values
+			},
+			upstream: &mockApisonator{
+				callback: func(request threescale.Request) {
+					expect := threescale.Request{
+						Auth: api.ClientAuth{
+							Type:  api.ProviderKey,
+							Value: providerKey,
+						},
+						Service: service,
+						Transactions: []api.Transaction{
+							{
+								Params: api.Params{
+									UserKey: user,
+								},
+							},
+						},
+					}
+					equals(t, expect, request)
+				},
+				result: &threescale.AuthorizeResult{
+					Authorized: false,
+					ErrorCode:  "service_id_invalid",
+				},
+			},
+			expectCode:        convert.CodeToStatusCode("service_id_invalid"),
+			expectHeaderValue: "service_id_invalid",
+		},
+		{
+			name: "Test success",
+			queryBuilder: func() url.Values {
+				values := url.Values{}
+				values.Add("user_key", user)
+				values.Add("service_id", service)
+				values.Add("provider_key", providerKey)
+				return values
+			},
+			upstream: &mockApisonator{
+				callback: func(request threescale.Request) {
+					expect := threescale.Request{
+						Auth: api.ClientAuth{
+							Type:  api.ProviderKey,
+							Value: providerKey,
+						},
+						Service: service,
+						Transactions: []api.Transaction{
+							{
+								Params: api.Params{
+									UserKey: user,
+								},
+							},
+						},
+					}
+					equals(t, expect, request)
+				},
+				result: &threescale.AuthorizeResult{Authorized: true},
+			},
+			expectCode: http.StatusOK,
+		},
 	}
-	decoder := xml.NewDecoder(w.Result().Body)
-	xml := &authResponseXML{}
-	decoder.Decode(xml)
 
-	if xml.Authorized != true {
-		t.Error("expected to be authorized")
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			req := httptest.NewRequest("GET", "/transactions/authorize.xml", nil)
+			req.URL.RawQuery = test.queryBuilder().Encode()
+
+			srv := newTestServer(t, test.upstream)
+			w := httptest.NewRecorder()
+			srv.router.ServeHTTP(w, req)
+
+			equals(t, test.expectCode, w.Code)
+			equals(t, test.expectHeaderValue, w.Header().Get(rejectionHeader))
+		})
 	}
 }
 
 func Test_handleAuthRep(t *testing.T) {
-	srv := newServer()
-	req := httptest.NewRequest("GET", "/transactions/authrep.xml", nil)
-	w := httptest.NewRecorder()
-
-	srv.router.ServeHTTP(w, req)
-	if w.Code != http.StatusOK {
-		t.Errorf("unexpected status code, wanted 200 but got %d", w.Code)
+	tests := []struct {
+		name              string
+		queryBuilder      func() url.Values
+		upstream          *mockApisonator
+		expectCode        int
+		expectHeaderValue string
+	}{
+		{
+			name: "Test upstream error results in failure",
+			queryBuilder: func() url.Values {
+				values := url.Values{}
+				values.Add("app_id", application)
+				values.Add("service_id", service)
+				values.Add("service_token", serviceToken)
+				return values
+			},
+			upstream: &mockApisonator{
+				callback: func(request threescale.Request) {
+					expect := threescale.Request{
+						Auth: api.ClientAuth{
+							Type:  api.ServiceToken,
+							Value: serviceToken,
+						},
+						Service: service,
+						Transactions: []api.Transaction{
+							{
+								Params: api.Params{
+									AppID: application,
+								},
+							},
+						},
+					}
+					equals(t, expect, request)
+				},
+				err: errors.New("some error"),
+			},
+			expectCode: http.StatusBadGateway,
+		},
+		{
+			name: "Test upstream denial fails to authorize",
+			queryBuilder: func() url.Values {
+				values := url.Values{}
+				values.Add("user_key", user)
+				values.Add("service_id", service)
+				values.Add("provider_key", providerKey)
+				return values
+			},
+			upstream: &mockApisonator{
+				callback: func(request threescale.Request) {
+					expect := threescale.Request{
+						Auth: api.ClientAuth{
+							Type:  api.ProviderKey,
+							Value: providerKey,
+						},
+						Service: service,
+						Transactions: []api.Transaction{
+							{
+								Params: api.Params{
+									UserKey: user,
+								},
+							},
+						},
+					}
+					equals(t, expect, request)
+				},
+				result: &threescale.AuthorizeResult{
+					Authorized: false,
+					ErrorCode:  "service_id_invalid",
+				},
+			},
+			expectCode:        convert.CodeToStatusCode("service_id_invalid"),
+			expectHeaderValue: "service_id_invalid",
+		},
+		{
+			name: "Test success",
+			queryBuilder: func() url.Values {
+				values := url.Values{}
+				values.Add("user_key", user)
+				values.Add("service_id", service)
+				values.Add("provider_key", providerKey)
+				return values
+			},
+			upstream: &mockApisonator{
+				callback: func(request threescale.Request) {
+					expect := threescale.Request{
+						Auth: api.ClientAuth{
+							Type:  api.ProviderKey,
+							Value: providerKey,
+						},
+						Service: service,
+						Transactions: []api.Transaction{
+							{
+								Params: api.Params{
+									UserKey: user,
+								},
+							},
+						},
+					}
+					equals(t, expect, request)
+				},
+				result: &threescale.AuthorizeResult{Authorized: true},
+			},
+			expectCode: http.StatusOK,
+		},
 	}
-	decoder := xml.NewDecoder(w.Result().Body)
-	xml := &authResponseXML{}
-	decoder.Decode(xml)
 
-	if xml.Authorized != true {
-		t.Error("expected to be authorized")
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			req := httptest.NewRequest("GET", "/transactions/authrep.xml", nil)
+			req.URL.RawQuery = test.queryBuilder().Encode()
+
+			srv := newTestServer(t, test.upstream)
+			w := httptest.NewRecorder()
+			srv.router.ServeHTTP(w, req)
+
+			equals(t, test.expectCode, w.Code)
+			equals(t, test.expectHeaderValue, w.Header().Get(rejectionHeader))
+		})
+	}
+}
+
+func TestNewServer(t *testing.T) {
+	srv, err := NewServer("http://3scale.net")
+	if err != nil {
+		t.Errorf("unexpected error %v", err)
+	}
+
+	_, ok := srv.upstreamPeer.(*backend.Backend)
+	if !ok {
+		t.Error("unexpected result from type assertion")
+	}
+
+}
+
+func newTestServer(t *testing.T, upstream *mockApisonator) *Server {
+	t.Helper()
+	srv := &Server{
+		router:       http.NewServeMux(),
+		upstreamPeer: upstream,
+	}
+	srv.registerRoutes()
+	return srv
+}
+
+type mockApisonator struct {
+	callback func(request threescale.Request)
+	result   *threescale.AuthorizeResult
+	err      error
+}
+
+func (m *mockApisonator) Authorize(request threescale.Request) (*threescale.AuthorizeResult, error) {
+	return m.auth(request)
+}
+
+func (m *mockApisonator) AuthRep(request threescale.Request) (*threescale.AuthorizeResult, error) {
+	return m.auth(request)
+}
+
+func (m mockApisonator) auth(request threescale.Request) (*threescale.AuthorizeResult, error) {
+	if m.callback != nil {
+		m.callback(request)
+	}
+	if m.err != nil {
+		return nil, m.err
+	}
+	return m.result, nil
+}
+
+func (m mockApisonator) Report(request threescale.Request) (*threescale.ReportResult, error) {
+	panic("implement me")
+}
+
+func (m mockApisonator) GetPeer() string {
+	panic("implement me")
+}
+
+// equals fails the test if exp is not equal to act.
+func equals(t *testing.T, exp, act interface{}) {
+	if !reflect.DeepEqual(exp, act) {
+		_, file, line, _ := runtime.Caller(1)
+		fmt.Printf("\033[31m%s:%d:\n\n\texp: %#v\n\n\tgot: %#v\033[39m\n\n", filepath.Base(file), line, exp, act)
+		t.FailNow()
 	}
 }
